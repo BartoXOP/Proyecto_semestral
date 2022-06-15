06312fcef0641f52846d4c33d1ded5f1f594af4c
josebarbozaDUOC <jo.barboza@duocuc.cl>
2022-06-15 18:41:07 -0400

    django_ montando proyecto base_002
    migración inicial, carpeta producto

--- petStoreApp/petStoreApp/settings.py
+++ petStoreApp/petStoreApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'producto',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
