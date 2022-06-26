68598d9cd7dfb40a208c62f825e9be39f173310b
josebarbozaDUOC <jo.barboza@duocuc.cl>
2022-06-26 15:10:24 -0400

    creando VIEWS genéricas, y templates
    creando VIEWS genéricas, y templates

--- petStoreApp/Mascota/urls.py
+++ petStoreApp/Mascota/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-#from .views import home
+from .views import PerritoCreate, PerritoList, PerritoUpdate, PerritoDelete
 
 urlpatterns = [
-    
+    path('add_perrito', PerritoCreate.as_view(), name="add_perrito"),
+    path('list_perritos/', PerritoList.as_view(), name='list_perritos'),
+    path('edit_perrito/<int:pk>', PerritoUpdate.as_view(), name='edit_perrito'),
+    path('del_perrito/<int:pk>', PerritoDelete.as_view(), name='del_perrito'),
 ]
--- petStoreApp/Mascota/views.py
+++ petStoreApp/Mascota/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import Http404
+from django.http import HttpResponse, Http404
 from .models import Perrito, Raza
 from .forms import PerritoForm
 from datetime import datetime, timedelta
 from django.utils import timezone
 
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+
 # Create your views here.
 
+#------------------------------Vistas clases genéricas
+class PerritoCreate(CreateView):
+    model = Perrito
+    form_class = PerritoForm
+    template_name = 'Mascotas/perrito_form.html'
+    success_url = reverse_lazy("list_perritos")
+
+class PerritoList(ListView):
+    model = Perrito
+    template_name = 'Mascotas/list_perritos.html'
+    # paginate_by = 4
+
+class PerritoUpdate(UpdateView):
+    model = Perrito
+    form_class = PerritoForm
+    template_name = 'Mascotas/perrito_form.html'
+    success_url = reverse_lazy('list_perritos')
+
+class PerritoDelete(DeleteView):
+    model = Perrito
+    template_name = 'Mascotas/perrito_delete.html'
+    success_url = reverse_lazy('list_perritos')
